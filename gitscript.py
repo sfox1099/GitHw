e3a34752a6d08c1d2242d537772fc05621c9e2d9
sfox1099 <sfox1099@gmail.com>
2023-10-30 22:28:07 -0400

    Add my greeting scrpit

---
+++ gitscript.py
@@ -0,0 +1,10 @@
+def greeting(lang):
+    lang = input("Enter Language:")
+    if lang == "Spanish":
+        print("Hola!")
+    if lang == "English":
+        print("Hello!")
+    if lang == "French":
+        print("Bonjour!")
+    print(lang)
+    
